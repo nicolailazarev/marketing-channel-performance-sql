7002723d8cc323424aefea48ab7f69c2565bf3e1
nicolailazarev <nila2657@colorado.edu>
2026-03-02 17:53:05 -0700

    Update 01_total_sessions.sql

--- SQL/01_total_sessions.sql
+++ SQL/01_total_sessions.sql
@@ -1,6 +1,7 @@
 -- Query 1: Total Sessions
 -- Purpose: Establish total session volume for the analysis period.
 -- Date Range: August 1, 2016 – August 1, 2017
+-- Result: 903,653 total sessions
 
 SELECT
   COUNT(*) AS total_sessions
