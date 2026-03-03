8ddcb78aa275d7aef6c010aae470c05c4d8b248b
nicolailazarev <nila2657@colorado.edu>
2026-03-02 17:51:00 -0700

    Create 01_total_sessions.sql
    Add total sessions query

---
+++ SQL/01_total_sessions.sql
@@ -0,0 +1,10 @@
+-- Query 1: Total Sessions
+-- Purpose: Establish total session volume for the analysis period.
+-- Date Range: August 1, 2016 – August 1, 2017
+
+SELECT
+  COUNT(*) AS total_sessions
+FROM
+  `bigquery-public-data.google_analytics_sample.ga_sessions_*`
+WHERE
+  _TABLE_SUFFIX BETWEEN '20160801' AND '20170801';
